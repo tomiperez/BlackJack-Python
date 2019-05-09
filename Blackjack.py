54aa0e8cd2dde59882b5e179749eb47cdb321be6
Tomas Perez <tommii.perez@gmail.com>
2019-05-09 18:36:36 -0300

    fix experimentar

--- Blackjack.py
+++ Blackjack.py
@@ -3,6 +3,7 @@ import random
 mazo = []
 jugadores = []
 ganadores = []
+
 def generar_mazos(n):
     global mazo
     if n>0:
@@ -22,7 +23,7 @@ def jugar(m):
         a = 0
         while (a < 21):
             a += mazo.pop(0)
-            #print (a)
+
             if (a == 21):
                 return a
 
@@ -30,11 +31,13 @@ def jugar(m):
                 return a
     else:
         return 0
+
 def jugar_varios(m, j):
     global jugadores
     for i in range(j):
         jugadores.insert(i,jugar(m))
     return mazo
+
 def ver_quien_gano(j):
     global jugadores
     for i in range(len(jugadores)):
@@ -42,22 +45,24 @@ def ver_quien_gano(j):
             print ("el jugador " + str(i+1) + " gano con " + str(jugadores[i]) + " pts.")
         else:
             print ("el jugador " + str(i+1) + " no gano, saco " + str(jugadores[i]) + " pts.")
+
 def ver_cartas_finales():
     print (len(mazo))
     return len(mazo)
+
 def experimentar (rep,n):
     global ganadores
     for p in range (n):
         ganadores.insert(p, 0)
     for i in range (rep):
         for j in range (n):
-            jugadores.insert(i, jugar(mazo))
-            if (jugadores[i]==21):
-                ganadores[i] = ganadores [i] + 1
+            jugadores.insert(j, jugar(mazo))
+            if (jugadores[j]==21):
+                ganadores[j] = ganadores [j] + 1
     for l in range (len(ganadores)):
         print ("El jugador " + str(l+1) + " gano " + str(ganadores[l]) + " veces.")
 
 
-#generar_mazos(4)gut
-#experimentar (3, 4)
-#VER_CARTAS_FINALES()
+generar_mazos(4)
+experimentar (3, 4)
+ver_cartas_finales()
